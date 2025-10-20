98c4c57f23547c8112a97252e6dd3dd07a280d93
LucianGheorghe95 <gheorghe.lucian1995@yahoo.com>
2025-10-20 22:07:08 +0300

    creare clasa ClubFotbal

---
+++ clubFotbal.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class ClubFotbal {
+private:
+    string nume;
+    string oras;
+    int anInfiintare;
+    int numarTrofee;
+
+public:
+
+};
+
+int main() {
+
+
+    return 0;
+}
