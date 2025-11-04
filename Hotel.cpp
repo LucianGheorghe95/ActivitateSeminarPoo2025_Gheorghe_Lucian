b0a782ef2b1a0e58940112ead61a4ffbc7f406d0
LucianGheorghe95 <gheorghe.lucian1995@yahoo.com>
2025-11-04 21:15:33 +0200

    metode de get si set si metoda de verificare camere disponibile

--- Hotel.cpp
+++ Hotel.cpp
@@ -25,10 +25,29 @@ public:
         numarCamere = totalCamere;
         camereOcupate = ocupate;
     }
+
+    // met de set
+    void setNume(string n){ numeHotel = n; }
+    void setOras(string o){ oras = o;}
+    void setNumarCamere(int nr){numarCamere = nr;}
+    void setCamereOcupate(int nr) {camereOcupate = nr;}
+
+    // met de get
+    string getNume(){return numeHotel;}
+    string getOras() { return oras; }
+    int getNumarCamere(){ return numarCamere;}
+    int getCamereOcupate() {return camereOcupate; }
+
+    // met de verificat nr camere disponibile
+    int camereLibere() {
+        return numarCamere - camereOcupate;
+    }
+
 };
 
 int main() {
 
+    Hotel h1("Caro", "Bucuresti", 200, 150);
 
     return 0;
 }
