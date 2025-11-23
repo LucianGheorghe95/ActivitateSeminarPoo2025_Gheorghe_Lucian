6909935a7224c355e2ca8ca211faae9685952ec1
LucianGheorghe95 <gheorghe.lucian1995@yahoo.com>
2025-11-23 20:11:31 +0200

    creare clasa concert si cu atributele aferente

---
+++ Concert.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Concert {
+private:
+    string numeConcert;
+    string locatie;
+    string data;
+    int numarSpectatori;
+public:
+};
+
+int main() {
+
+    return 0;
+}
