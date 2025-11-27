9ae569433c322f89a54d0290a95bf402b7bc9b96
LucianGheorghe95 <gheorghe.lucian1995@yahoo.com>
2025-11-27 18:17:21 +0200

    metoda afisare si apelare constructori si metoda afisare in main

--- Concert.cpp
+++ Concert.cpp
@@ -22,9 +22,24 @@ public:
         data = d;
         numarSpectatori = nr;
     }
+
+    void afiseazaInfo() const {
+        cout << "Concert: " << numeConcert << endl;
+        cout << "Locatie: " << locatie << endl;
+        cout << "Data: " << data << endl;
+        cout << "Numar spectatori: " << numarSpectatori << endl;
+    }
 };
 
 int main() {
+
+    Concert c1;
+    cout << "Concert implicit" << endl;
+    c1.afiseazaInfo();
+
+    Concert c2("Untold", "Cluj", "04.08.2025", 75000);
+    cout << "\nConcert 2:" << endl;
+    c2.afiseazaInfo();
 
     return 0;
 }
