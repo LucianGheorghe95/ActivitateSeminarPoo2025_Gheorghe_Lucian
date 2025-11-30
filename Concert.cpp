45c35a7e4009e2d1acb0103c34887e47c7f2b375
LucianGheorghe95 <gheorghe.lucian1995@yahoo.com>
2025-11-30 22:41:51 +0200

    metoda de calcualae a venitului total

--- Concert.cpp
+++ Concert.cpp
@@ -8,19 +8,26 @@ private:
     string locatie;
     string data;
     int numarSpectatori;
+    double pretBilet;   
 public:
     Concert() {
         numeConcert = "nu stiu";
         locatie = "locatie";
         data = "data";
         numarSpectatori = 0;
+        pretBilet = 0.0;
     }
 
-    Concert(string nume, string loc, string d, int nr) {
+    Concert(string nume, string loc, string d, int nr, double pret) {
         numeConcert = nume;
         locatie = loc;
         data = d;
         numarSpectatori = nr;
+        pretBilet = pret;
+    }
+
+    double venitTotal() const {
+        return numarSpectatori * pretBilet;
     }
 
     void afiseazaInfo() const {
@@ -28,6 +35,8 @@ public:
         cout << "Locatie: " << locatie << endl;
         cout << "Data: " << data << endl;
         cout << "Numar spectatori: " << numarSpectatori << endl;
+        cout << "Pret bilet: " << pretBilet << " lei" << endl;
+        cout << "Venit total estimat: " << venitTotal() << " lei" << endl; 
     }
 };
 
@@ -37,7 +46,7 @@ int main() {
     cout << "Concert implicit" << endl;
     c1.afiseazaInfo();
 
-    Concert c2("Untold", "Cluj", "04.08.2025", 75000);
+    Concert c2("Untold", "Cluj", "04.08.2025", 75000, 350.5);
     cout << "\nConcert 2:" << endl;
     c2.afiseazaInfo();
 
